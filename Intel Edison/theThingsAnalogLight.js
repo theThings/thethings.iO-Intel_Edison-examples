aa37a033af36e3c0f07b96a32ad6c11dc6ac24be
andreshcasaus <a.hernandez.casaus@gmail.com>
2015-02-25 11:14:57 +0100

    Update theThingsAnalogLight.js

--- Intel Edison/theThingsAnalogLight.js
+++ Intel Edison/theThingsAnalogLight.js
@@ -4,10 +4,8 @@ var light = new groveSensor.GroveLight(0);
 
 var theThingsAPI = require('thethingsio-api')
 
-//create Client
 var client = theThingsAPI.createClient()
 
-//The object to write.
 var object = {
     "values": [
         {
@@ -18,7 +16,6 @@ var object = {
 }
 
 client.on('ready', function () {
-//write the object
     setInterval(function () {
         object.values[0].value = light.value()
         client.thingWrite(object, function (error, data) {
